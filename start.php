37b55a77e50968dc194b4776f2e55d2013cb50bf
Sofyan <sofyan@sd-142033.dedibox.fr>
2023-10-30 16:44:26 +0100

    doctrine dans start.php

--- start.php
+++ start.php
@@ -2,14 +2,33 @@
     require_once 'vendor/autoload.php';
     session_start();
 
+    use Doctrine\ORM\Tools\Setup;
+    use Doctrine\ORM\EntityManager;
+
+    $paths = array("Entity" , "toto");
+    $isDevMode = true;
+    $proxyDir=null;
+    $cache=null;
+
+    $dbParams = array (
+        'driver' => 'pdo_mysql',
+        'user' => 'sofyan',
+        'password' => 'plop',
+        'dbname' => 'BTS'
+    );
+    $useSimpleAnnotationReader = false;
+
+
+
+
     $class = "Controllers\\" . (isset($_GET['c'])? ucfirst($_GET['c']) . 'Controller' : 'UserController');
     $target = isset($_GET['t']) ? $_GET['t'] : "index";
     $getParams = isset($_GET) ? $_GET : null;
     $postParams = isset($_POST) ? $_POST : null;
 
     $params = array(
-        $getParams,
-        $postParams,
+        //$getParams,
+        //$postParams,
         "http://195.154.118.169/myFramework/",
     );
 
